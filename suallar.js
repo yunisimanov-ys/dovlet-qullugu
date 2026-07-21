b46b072d771962cde447631abde46508607ad4d3
Dq-Platform <agasifimanov@gmail.com>
2026-07-21 14:05:29 +0400

    Create suallar.js

---
+++ suallar.js
@@ -0,0 +1,34 @@
+const suallar = [
+{
+sual: "Azərbaycan Respublikasının Konstitusiyası neçənci ildə qəbul olunub?",
+variantlar: [
+"1991",
+"1993",
+"1995",
+"2000"
+],
+cavab: "1995"
+},
+
+{
+sual: "Azərbaycanın paytaxtı hansıdır?",
+variantlar: [
+"Gəncə",
+"Bakı",
+"Şəki",
+"Sumqayıt"
+],
+cavab: "Bakı"
+},
+
+{
+sual: "Dövlət qulluğu imtahanını hansı qurum təşkil edir?",
+variantlar: [
+"DİM",
+"ETN",
+"Nazirlər Kabineti",
+"Prezident Administrasiyası"
+],
+cavab: "DİM"
+}
+];
